27e698477a18353b1932fdd04c6053e6686e533a
bamichoi <cbs412@naver.com>
2023-08-20 12:50:06 +0900

    solved beakjoon:2748

--- baekjoon/2748.js
+++ baekjoon/2748.js
@@ -3,14 +3,13 @@ const path = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 const input = fs.readFileSync(path).toString();
 
 const n = parseInt(input);
+let dp = new Array(91).fill(null);
 
-let ans = 0;
-const arr = [0, 1];
+dp[0] = 0;
+dp[1] = 1;
 
-for (i = 2; i <= n; i++) {
-  const pibo = BigInt(arr[arr.length - 1]) + BigInt(arr[arr.length - 2]);
-  arr.push(pibo);
-  ans = pibo;
+for (let i = 2; i <= n; i++) {
+  dp[i] = BigInt(dp[i - 1]) + BigInt(dp[i - 2]);
 }
 
-console.log(ans);
+console.log(String(dp[n]));
