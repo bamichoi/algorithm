b0fcd971f59f328fed25f9599a5c892b73645f61
beomseok.choi <cbs412@naver.com>
2023-08-21 12:27:12 +0900

    boj 10984 add example using readline

--- baekjoon/10984.js
+++ baekjoon/10984.js
@@ -20,3 +20,36 @@ for (i = 0; i < t; i++) {
   }
   console.log(c, (g / c).toFixed(1));
 }
+
+// const readline = require("readline");
+// const rl = readline.createInterface({
+// 	input: process.stdin,
+// 	output: process.stdout,
+// });
+
+// let input = [];
+
+// rl.on("line", (line) => {
+// 	input.push(line);
+// });
+
+// rl.on("close", () => {
+// 	let ans = [];
+//     for (let i = 1; i < input.length; i++) {
+//         let len = +input[i];
+//         let sum = [0, 0];
+
+//         for (let j = 0; j < len; j++) {
+//             let c = input[i+j+1].split(' ').map(v=>+v);
+//             sum[0]+=c[0];
+//             sum[1]+=c[0]*c[1];
+//         }
+//         sum[1] = (sum[1]/sum[0]).toFixed(1);
+
+//         ans.push(sum.join(' '))
+//         i+=len;
+//     }
+
+//     console.log(ans.join('\n'));
+// 	process.exit();
+// });
