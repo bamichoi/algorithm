891c6ee8a8d915461488fd149650f67455691423
beomseok.choi <cbs412@naver.com>
2023-08-22 10:52:28 +0900

    solved boj 2444

---
+++ baekjoon/2444.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const path = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+const input = fs.readFileSync(path).toString();
+
+const n = +input;
+let ans = "";
+
+for (let i = 1; i <= n; i++) {
+  ans += " ".repeat(n - i) + "*".repeat(2 * i - 1) + "\n";
+}
+for (let i = n - 1; i > 0; i--) {
+  ans += " ".repeat(n - i) + "*".repeat(2 * i - 1) + "\n";
+}
+
+console.log(ans);
