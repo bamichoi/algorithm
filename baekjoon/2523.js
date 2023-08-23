3a17d4a98339bf666bdd18241f098b0c61007e23
beomseok.choi <cbs412@naver.com>
2023-08-23 09:56:01 +0900

    solved boj 2523

---
+++ baekjoon/2523.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const path = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+const input = fs.readFileSync(path).toString();
+
+const n = +input;
+let ans = "";
+
+for (let i = 0; i < n + (n - 1); i++) {
+  const a = Math.abs(n - i - 1);
+  const b = Math.abs(a - n);
+  ans += "*".repeat(b) + "\n";
+}
+
+console.log(ans);
