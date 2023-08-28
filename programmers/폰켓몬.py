62e6ebd8aa607ee8a14deba28452556dc4699387
beomseok.choi <cbs412@naver.com>
2023-08-28 13:47:49 +0900

    solved codeup 6001-6008, programmers 폰켓몬

---
+++ programmers/폰켓몬.py
@@ -0,0 +1,16 @@
+from itertools import combinations
+
+
+def solution(nums):
+    answer = 0
+
+    t = len(nums) / 2
+    
+    dic = dict.fromkeys(nums)
+
+    if t <= len(dic):
+        answer = t
+    else:
+        answer = len(dic)
+
+    return answer
